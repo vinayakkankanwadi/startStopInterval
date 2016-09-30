30f1010204163dd116c3b2ac1454989adbcdb0b7
Vinayak <vinayakkankanwadi@hotmail.com>
2016-10-01 07:51:28 +1000

    Take one on running gulp tests on check in

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var bump = require('gulp-bump');
 var git = require('gulp-git');
+var mocha  = require('gulp-mocha');
 
+gulp.task('test', function() {
+  return gulp
+    .src('test/*.js')
+    .pipe(mocha());
+});
 
-gulp.task('pre-commit', function() {
+gulp.task('pre-commit', ['test'], function() {
   console.log('Bumpig up the version');
   gulp.src(['./package.json'])
   .pipe(bump({type:'patch'}))
