0263357c0bc3162e0affbd079b36078d74df4a42
Vinayak <vinayakkankanwadi@hotmail.com>
2016-10-01 07:14:09 +1000

    added proper unit test for startStopInterval

--- test/test.js
+++ test/test.js
@@ -1,11 +1,23 @@
 var chai = require('chai');
 var expect = chai.expect;
+var sinon = require('sinon');
+var assert = require('assert');
+// Include our function as well
+var ssi = require('..');
 
-describe('Dummy suite of tests', function() {
-  it('should 1 be equal to 1', function() {
-    expect(1).to.equal(1);
-  });
-  it('should 2 be greater than 0', function() {
-    expect(2).to.be.above(0);
+describe('startStopInterval', function() {
+  it('calls startStopInterval function', function() {
+	var clock = sinon.useFakeTimers();
+    var spy = sinon.spy();
+    ssi.startStopInterval(true,spy,2);
+	assert(! spy.calledOnce);
+    clock.tick(2);
+    assert(spy.calledOnce);
+	clock.tick(2);
+    assert(spy.calledTwice);
+	ssi.startStopInterval(false); // Stop
+	clock.tick(2);
+    assert(! spy.calledThrice);
+    clock.restore();
   });
 });
