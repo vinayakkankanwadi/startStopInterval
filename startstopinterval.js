3eb47bf20c3ed08fbdfdb45763d538f8dbf066fb
Vinayak <vinayakkankanwadi@hotmail.com>
2016-09-30 18:14:16 +1000

    Cleaned up code a bit

--- startstopinterval.js
+++ startstopinterval.js
@@ -1,6 +1,11 @@
-function startStopInterval(start,fn, interval=1000) {
+function startStopInterval(start,fn,interval) {
  if ( start ) {
   if (startStopInterval.Id) return
+
+  // Old way before ES6 (EcmaScript)
+  if (interval === undefined) {
+   interval = 1000;
+  } 	
   startStopInterval.Id = setInterval(fn, interval)
  }
  else {
