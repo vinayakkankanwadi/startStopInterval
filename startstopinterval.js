83c7d561e5eaad5074372e3f226898948ff6b1c6
Vinayak <vinayakkankanwadi@hotmail.com>
2016-10-08 08:17:22 +1000

    refac

--- startstopinterval.js
+++ startstopinterval.js
@@ -4,8 +4,10 @@
 //----------------------------------------------------------------------------- 
 function hashCode(str){
 	var hash = 0;
-    if (str.length === 0) 
+    if (str.length === 0)
+    {		
 		return hash;
+    }
     for (i = 0; i < str.length; i++) {
         char = str.charCodeAt(i);
         hash = ((hash<<5)-hash)+char;
